16b42096d450dbf74e802249374452c13e1812fd
romalley696 <111869119+romalley696@users.noreply.github.com>
2023-02-07 13:09:00 +0000

    1st javascript work

---
+++ Basic_JavaScript_Projects/JS/main.js
@@ -0,0 +1,44 @@
+alert("hello world!"); //pop up window alert
+
+window.alert("hello my friends"); //pop up window alert
+
+document.write("what's up!<br><br>"); // text on page
+
+var A="this is a string"; //text from string
+document.write(A)
+
+var B="I am learning to write strings"; //pop up window alert using stings
+window.alert(B);
+
+document.write(" <br><br>I'm learning to write special cases like quotations \"why\" this is all using \\ backslashes before a special character <br><br>"); //text using special cases
+
+document.write("\"this is my quotation about," // using concatenating
++" how to join text using concatenating\"");
+
+var C="i'm using variables as strings";
+document.write("<br><br>i am trying to link a concatenated string <br>" // using strings concatenating
++C
++"<br><br>")
+
+var family="O\'Malley", dad="Richard", mum="Natalie", son1="Brendon", //defining variables
+son2="Connor", daughter="Melissa", son3="Liam";
+var family = family.fontcolor("green"); //colouring variables
+var dad=dad.fontcolor("red");
+var son1=son1.fontcolor("green")
+var son2=son2.fontcolor("orange")
+var son3=son3.fontcolor("blue")
+var daughter=daughter.fontcolor("yellow")
+var mum=mum.fontcolor("pink")
+document.write(family); // displaying coloured text
+document.write(dad);
+
+document.write("<br><br>") //spacing 
+
+document.write(8+2) //using expressions
+
+document.write("<br><br>") //spacing
+
+let num1="13"; //defining variables
+let num2="23";
+let sum = num1 + num2;
+document.write(sum); //using variables using expressions
---
+++ README.md
@@ -0,0 +1,4 @@
+This is the repository will contain my work on JavaScript.
+
+
+
