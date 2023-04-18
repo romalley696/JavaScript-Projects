9ec786346e9a82fce58c4421dd0b7991a0b45fa2
romalley696 <111869119+romalley696@users.noreply.github.com>
2023-04-18 13:57:09 +0100

    challenges

---
+++ Challenges/challenges.js
@@ -0,0 +1,19 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
